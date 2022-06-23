db80553a1d28d68d58495d7b4585fed9984fdb30
Irem Gunay <iremguunay@gmail.com>
2022-06-24 00:14:23 +0300

    Usage of react memoization

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header({number}) {
+    console.log("Header Component re-rendered");
+  return (
+    <div>Header - {number}</div>
+  )
+}
+
+export default React.memo(Header)
